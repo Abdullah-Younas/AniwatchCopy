24e19f1d4462a990d424fe2a71d1cea1289fe3af
Abdullah Younas <abdullahyounas24082005@gmail.com>
2023-12-19 21:34:59 +0500

    ANIC2

--- ani-2-watchC/src/MHeader.jsx
+++ ani-2-watchC/src/MHeader.jsx
@@ -30,7 +30,7 @@ function MHEADER(){
                         </p>
                     </div>
                     <div className='second-container'>
-                        <img src= "\public\img\aniCharmin.png" alt='img'/>
+                        <img src= "\public\aniCharmin.png" alt='img'/>
                     </div>
             </div>
             </div>
